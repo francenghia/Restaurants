55e8dfafd022adb9f24d56990cd48cd4d19e1301
francenghia <francenghia@gmail.com>
2020-04-01 10:16:08 +0700

    [Common][Fix] create screen customer and fix permission denied when open gallery

---
+++ customer/src/main/java/com/example/customer/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.customer.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.customer.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- customer/src/main/java/com/example/customer/Activities/MainActivity.java
+++ customer/src/main/java/com/example/customer/Activities/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.customer.Activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.customer.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.common.Shared.ROOT_UID;
 
 public class MainActivity extends AppCompatActivity {
     private String email, password, errMsg;
@@ -13,5 +20,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser()!= null){
+            ROOT_UID = auth.getUid();
+
+            Intent i = new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(i);
+
+            finish();
+        }
+
+        findViewById(R.id.sign_up).setOnClickListener(e -> {
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivityForResult(i,1);
+        });
+
+        findViewById(R.id.sign_in).setOnClickListener(e -> {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Authenticating...");
+            if(checkFields()){
+
+                progressDialog.show();
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                ROOT_UID = auth.getUid();
+                                Intent fragment = new Intent(this, HomeActivity.class);
+                                startActivity(fragment);
+                                progressDialog.dismiss();
+                                finish();
+                            } else {
+                                Toast.makeText(MainActivity.this,"Wrong Username or Password", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(MainActivity.this, errMsg, Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        });
+
+
     }
+
+    public boolean checkFields(){
+        email = ((EditText)findViewById(R.id.email)).getText().toString();
+        password = ((EditText)findViewById(R.id.password)).getText().toString();
+
+        if(email.trim().length() == 0 || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            errMsg = "Invalid Mail";
+            return false;
+        }
+
+        if(password.trim().length() == 0){
+            errMsg = "Fill password";
+            return false;
+        }
+
+        return true;
+    }
+
 }
