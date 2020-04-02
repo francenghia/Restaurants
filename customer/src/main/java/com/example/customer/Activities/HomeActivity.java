0ff47fc4d1e3c2a7bde3b049b211e0adfa5e5a25
francenghia <francenghia@gmail.com>
2020-04-02 09:27:45 +0700

    [Common] create Profile customer

--- customer/src/main/java/com/example/customer/Activities/HomeActivity.java
+++ customer/src/main/java/com/example/customer/Activities/HomeActivity.java
@@ -1,16 +1,61 @@
 package com.example.customer.Activities;
 
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.customer.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements
+        RestaurantFragment.OnFragmentInteractionListener,
+        ProfileFragment.OnFragmentInteractionListener,
+        OrderFragment.OnFragmentInteractionListener {
+
+    public static final String PREFERENCE_NAME = "ORDER_LIST";
+
+    private SharedPreferences order_to_listen;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                //onRefuseOrder();
+                if (!(getSupportFragmentManager().findFragmentById(R.id.fragment_container) instanceof RestaurantFragment)) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RestaurantFragment()).commit();
+                }
+                return true;
+            case R.id.navigation_profile:
+                //onRefuseOrder();
+                if (!(getSupportFragmentManager().findFragmentById(R.id.fragment_container) instanceof ProfileFragment)) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                }
+                return true;
+            case R.id.navigation_reservation:
+                //onRefuseOrder();
+                if (!(getSupportFragmentManager().findFragmentById(R.id.fragment_container) instanceof OrderFragment)) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new OrderFragment()).commit();
+                }
+                return true;
+        }
+
+        return false;
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
