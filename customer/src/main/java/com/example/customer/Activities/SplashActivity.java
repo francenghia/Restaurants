85e5bfc4e465ce8d7899db5fb3d8313daa6c2a65
francenghia <francenghia@gmail.com>
2020-04-19 22:16:28 +0700

    [Create] create splash screen using lottie

--- customer/src/main/java/com/example/customer/Activities/SplashActivity.java
+++ customer/src/main/java/com/example/customer/Activities/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.example.shipper.Activities;
+package com.example.customer.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.shipper.R;
+import com.example.customer.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -44,6 +44,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(mIntent);
                 finish();
             }
-        },3000);
+        },3500);
     }
 }
--- restaurants/src/main/java/com/example/restaurants/Startup/SplashScreenActivity.java
+++ restaurants/src/main/java/com/example/restaurants/Startup/SplashScreenActivity.java
@@ -1,16 +1,48 @@
 package com.example.restaurants.Startup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.restaurants.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private TextView txtWelcome,txtAnim;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        txtWelcome=findViewById(R.id.txtWelcome);
+        txtAnim=findViewById(R.id.txtAnim);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.push_right);
+        txtWelcome.setAnimation(animation);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.push_left);
+        txtAnim.setAnimation(animation);
+
+        Handler handler = new Handler();
+        final Intent mIntent = new Intent(this, MainActivity.class);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(mIntent);
+                finish();
+            }
+        },2500);
     }
 }
